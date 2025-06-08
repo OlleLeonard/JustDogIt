29184ed29daaff09a26391efcf54a4b7bc58e635
OlleLeonard <olleleonard@gmail.com>
2025-06-08 19:15:14 +0200

    uppdate

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("kontaktform").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Tack! Vi har mottagit ditt meddelande.");
 });
+
+function toggleText() {
+  const text = document.getElementById("extraText");
+  text.style.display = text.style.display === "none" ? "block" : "none";
+}
